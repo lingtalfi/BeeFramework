3d24e17ed304ee38fe1fd295119b513c82fb237e
lingtalfi <lingtalfi@gmail.com>
2017-05-22 12:14:02 +0200

    update

--- Notation/String/ShortCode/Tool/ShortCodeTool.php
+++ Notation/String/ShortCode/Tool/ShortCodeTool.php
@@ -21,6 +21,79 @@ use BeeFramework\Notation\String\StringParser\ExpressionDiscoverer\Miscellaneous
  * @author Lingtalfi
  * 2015-05-09
  *
+ *
+ *
+ *
+ *
+ * 2017-05-22
+ * Example
+ * --------------
+ *
+ * az(ShortCodeTool::parse("hello=6, pou=[a, [b, c]], e='po=po', f='[pou]', g=[po => 4, go => [1,2,3], mo]"));
+ *
+ * array(5) {
+ *      ["hello"] => int(6)
+ *      ["pou"] => array(2) {
+ *          [0] => string(1) "a"
+ *          [1] => array(2) {
+ *              [0] => string(1) "b"
+ *              [1] => string(1) "c"
+ *          }
+ *      }
+ *      ["e"] => string(5) "po=po"
+ *      ["f"] => string(5) "[pou]"
+ *      ["g"] => array(3) {
+ *          ["po"] => int(4)
+ *          ["go"] => array(3) {
+ *               [0] => int(1)
+ *              [1] => int(2)
+ *              [2] => int(3)
+ *          }
+ *          [0] => string(2) "mo"
+ *      }
+ * }
+ *
+ *
+ * Shortcode notation
+ * ---------------
+ * The shortcode notation is composed of key/value pairs separated by comma.
+ * The double quotes and or single quotes can be used to escape special characters, such as the comma for instance
+ * (if the comma needs to be used as a value and not a key/value pair separator).
+ *
+ * The key is separated from the value by the equal symbol (=).
+ * Spaces around the equal symbol don't matter.
+ *
+ * So for instance, you would write key=value, key2=value2, key3 = value3, ...
+ *
+ * ### Arrays
+ * Shortcode notation let you write array, and even nested array.
+ *
+ * the array is a sequence of items separated by a comma.
+ * Each item can be in one of the following form:
+ *
+ * - itemKey => itemValue
+ * - itemValue
+ *
+ * Note that the "equal-greater than" symbols combo (=>) is used to separate the itemKey from the itemValue.
+ * An itemValue can also be an array.
+ *
+ * So, now you know the shortcode notation, enjoy!
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 class ShortCodeTool
 {
